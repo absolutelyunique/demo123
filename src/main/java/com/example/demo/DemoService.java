6e15d38051a6b638c1fe65e48204380c753d1d15
absolutelyunique <59395978+absolutelyunique@users.noreply.github.com>
2020-09-16 23:28:25 -0400

    Create DemoService.java

---
+++ src/main/java/com/example/demo/DemoService.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+public class DemoService {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World !!");
+		System.out.println("Testing sonar qube !!");
+		
+		for(int i=1; i>=1; i++){
+            System.out.println("The value of i is: "+i);
+       }
+	   
+	   int j;
+		while (true) { // Noncompliant; end condition omitted
+		  j++;
+		}
+	}
+
+}
